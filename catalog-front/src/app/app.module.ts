164498f973db7088777a969683514456686ac69e
Lauta <lautarolopezruiz@gmail.com>
2021-09-13 20:28:53 -0300

    add ngrx and moving scaffolding

--- catalog-front/src/app/app.module.ts
+++ catalog-front/src/app/app.module.ts
@@ -6,22 +6,22 @@ import { RouterModule } from '@angular/router';
 
 
 import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
+import { ComponentsModule } from './layout/components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { UserProfileComponent } from './layout/user-profile/user-profile.component';
+import { TableListComponent } from './layout/table-list/table-list.component';
+import { TypographyComponent } from './layout/typography/typography.component';
+import { IconsComponent } from './layout/icons/icons.component';
+import { MapsComponent } from './layout/maps/maps.component';
+import { NotificationsComponent } from './layout/notifications/notifications.component';
+import { UpgradeComponent } from './layout/upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AdminLayoutComponent } from './layout/main/admin-layout/admin-layout.component';
 
 @NgModule({
   imports: [
