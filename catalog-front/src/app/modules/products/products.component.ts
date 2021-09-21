d46e462d4343dcead7f0fd28ae8253752c089633
Lauta <lautarolopezruiz@gmail.com>
2021-09-21 15:33:37 -0300

    add products module

---
+++ catalog-front/src/app/modules/products/products.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {State} from '../../infrastructure/store/state';
+import {Observable} from 'rxjs';
+import * as ProductsCollection from '../../infrastructure/store/collections/products';
+import * as ProductStore from '../../infrastructure/store/products';
+import {Product} from '../../infrastructure/models/product.model';
+import {startWith} from 'rxjs/operators';
+
+@Component({
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+    products$: Observable<any>;
+
+    constructor(private store: Store<State>) {
+    }
+
+    ngOnInit(): void {
+        this.products$ = this.store.select(ProductStore.selectAll);
+    }
+
+    getRandomString(length) {
+        const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+        }
+        return result;
+    }
+
+    add() {
+        this.store.dispatch(ProductStore.Add({
+            product: {
+                name: this.getRandomString(8)
+            } as Product
+        }))
+    }
+
+    delete(id) {
+        this.store.dispatch(ProductStore.Delete({id}));
+    }
+}
